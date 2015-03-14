6711b23d1fefe010e5e2a2eea4b9a773ff9399c8
Sebastian Gräßl <sebastian@validcode.me>
2015-03-14 11:30:25 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    exphbs  = require('express-handlebars'),
+    app = express();
+
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
+app.use('/javascripts', express.static(__dirname + '/javascripts'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address,
+      port = server.address().port;
+
+  console.log('RossCon Logo app listening at http://%s:%s', host, port);
+
+});
---
+++ javascripts/logo_generator.js
@@ -0,0 +1,68 @@
+// SebastianGraessl
+
+function Logo() {
+  this.value;
+  this.container = $('<div id="logo"></div>');
+  this.alphabet  = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+  this.init = function() {};
+
+  this.setValue = function(value) {
+    this.value = value.toUpperCase();
+
+    this.replaceUmlauts();
+
+    console.log(this.value);
+
+    $('#logo').empty();
+
+    this.generateGrid();
+
+    $('body').append(this.container);
+  };
+
+  this.replaceUmlauts = function () {
+    this.value.replace(/Ää/g, "ae")
+              .replace(/Öö/g, "oe")
+              .replace(/Üü/g, "ue")
+              .replace(/ß/g, "ss");
+  };
+
+  this.generateGrid = function () {
+    var self = this;
+
+    _.forEach(this.alphabet, function (char) {
+      var block = $('<span></span>');
+
+      if (self.value.indexOf(char) !== -1) {
+        block.addClass('present');
+        block.css('opacity', self.calcOpacity(char));
+      }
+
+      $(self.container).append(block);
+    });
+
+    this.addMeta();
+  };
+
+  this.addMeta = function () {
+    $(this.container).append('<span class="filler"></span>');
+    $(this.container).append('<span></span>');
+    $(this.container).append('<span></span>');
+    $(this.container).append('<span class="filler"></span>');
+  };
+
+  this.calcOpacity = function (char) {
+    return 1/(this.value.match(new RegExp(char, "g")||[]).length);
+  };
+
+  this.init();
+}
+
+$(document).on('ready', function(){
+  var _logo = new Logo();
+
+  $('input[name=text]').on('keyup', function (){
+    _logo.setValue($(this).val());
+  });
+});
